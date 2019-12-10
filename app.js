b8813b2693191d721648a7cdbf7b575482866aaa
Tõnn <tonn.toomsalu@gmail.com>
2019-12-10 18:42:46 +0200

    Add LVL4 authentication. Salting and hashing passwords with bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 
 
@@ -42,32 +43,35 @@ app.get("/secrets", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
 
-    newUser.save((err) => {
-        if(err){
-            console.log(err);
-        } res.render("secrets.ejs");
-    });
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save((err) => {
+            if(err){
+                console.log(err);
+            } res.render("secrets.ejs");
+        });
+      });
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, (err, foundUser) => {
         if(err){
             console.log(err)
         } else {
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets.ejs");
-                } else {
-                    console.log("You entered a wrong password. Try again!")
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true){
+                        res.render("secrets.ejs");  
+                    }
+                });
             }
         }
     });
